3dfeee345c32b5fec5d0ca7eda100eff172447cd
hoangmy <mynguyen8333@gmail.com>
2021-10-19 20:52:58 +0700

    them bootrap3

--- service.js
+++ service.js
@@ -10,6 +10,10 @@ app.get('/',(req,res)=>{
     return res.render('index');
 });
 
+app.get('/trangchu',(req,res)=>{
+    return res.render('giaodiensinhvien');
+});
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
 });
